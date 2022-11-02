f85a93773d880ce5e3fe4016e5b131d9654af322
BT_4N2_22 <wlei6251@bths.edu>
2022-11-02 12:33:17 -0400

    Initial commit

---
+++ src/StringLoops.java
@@ -0,0 +1,36 @@
+public class StringLoops
+{
+    // default constructor; no instance variables
+    public StringLoops() { }
+
+    /* Returns the number of times "character" appears in "searchString"
+       This should be case sensitive!
+
+       Examples:
+       - if character = "a" and searchString = "Apples and bananas",
+         this method returns 4
+       - if character = "A" and searchString = "Apples and bananas",
+         this method returns 1
+       - if character = "!" and searchString = "Hello! Nice day!",
+         this method returns 2
+      */
+    public int countCharacters(String character, String searchString)
+    {
+        for (int i = 0; i < searchString.length(); i++)
+        {
+            
+        }
+    }
+
+    /* Returns the original string reversed
+
+     Examples:
+     - if origString = "hello!" this method returns "!olleh"
+     - if origString = "Apples and bananas" this method returns "sananab dna selppA"
+    */
+    public String reverseString(String origString)
+    {
+        /* to be implemented */
+    }
+}
+
---
+++ src/U4L5Runner.java
@@ -0,0 +1,11 @@
+public class U4L5Runner {
+    public static void main(String[] args) {
+        String print = "Print Me!";
+
+        for(int i = 0; i <= print.length(); i++)
+        {
+            System.out.println(print.substring(i, i + 1));
+        }
+
+    }
+}
